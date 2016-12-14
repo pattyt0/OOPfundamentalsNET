1ecdc0183b47ca508d557c3802c2c5ec621d21a4
pattyT0 <pattyopatito@gmail.com>
2016-12-13 23:51:49 -0400

    step 2

---
+++ ErrorHandling_Step1/step2_EH/step2_EH.aspx
@@ -0,0 +1,37 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="step2_EH.aspx.cs" Inherits="step2_EH.step2_EH" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        <asp:Label ID="Label1" runat="server" Text="Miles"></asp:Label>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:TextBox ID="txtMiles" runat="server" OnTextChanged="TextBox1_TextChanged"></asp:TextBox>
+        <br />
+        Galons&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:TextBox ID="txtGalons" runat="server"></asp:TextBox>
+        <br />
+        <br />
+        Efficiency&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:TextBox ID="txtEfficiency" runat="server"></asp:TextBox>
+        <br />
+        <br />
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:Button ID="btbCalculate" runat="server" OnClick="Button1_Click" Text="Calculate" />
+        <br />
+        <br />
+        <asp:Label ID="lblMessage" runat="server"></asp:Label>
+        <br />
+        <br />
+        <br />
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ ErrorHandling_Step1/step2_EH/step2_EH.aspx.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace step2_EH
+{
+    public partial class step2_EH : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void TextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                decimal miles = Decimal.Parse(txtMiles.Text);
+                decimal galons = Decimal.Parse(txtGalons.Text);
+                decimal efficiency = miles / galons;
+
+                txtEfficiency.Text = string.Format("{0:n}", efficiency);
+                    
+            }
+            catch (FormatException fe)
+            {
+                string msg = String.Format("Message: {0} <br> Stack Trace : <br> {1}", fe.Message, fe.StackTrace);
+                lblMessage.Text = fe.GetType().ToString() + "<br>" + msg;
+                
+            }
+            catch (DivideByZeroException dbze)
+            {
+                string msg = String.Format("Message: {0} <br> Stack Trace : <br> {1}", dbze.Message, dbze.StackTrace);
+                lblMessage.Text = dbze.GetType().ToString() + "<br>" + msg;
+                
+            }
+            catch (Exception ex)
+            {
+                string msg = String.Format("Message: {0} <br> Stack Trace : <br> {1}", ex.Message, ex.StackTrace);
+                lblMessage.Text = ex.GetType().ToString() + "<br>" + msg;
+                
+            }
+        }
+    }
+}
